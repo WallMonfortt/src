a609037047d08a033b5f9bcce067d104ecad48eb
Wal Monfortt <jose.gual.monfortte.f@gmail.com>
2021-10-20 11:25:15 -0500

    API v1 done

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
-// const {user} = require("./models");
 const userRoutes = require("./routes/users.routes")
+const cors = require('cors')
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/Api/v1",userRoutes);
 
